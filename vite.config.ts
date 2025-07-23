5ce4751d6c2e9f60bfd04783edda05d87182e8ab
Oleksandr Martynov <alex@Oleksandrs-MacBook-Air.local>
2025-07-23 23:00:55 +0300

    edit vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react(),tsconfigPaths()],
-  build: {
-    outDir: 'dist',
-  },
 })
